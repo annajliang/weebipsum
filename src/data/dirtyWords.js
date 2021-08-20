e1a4490c029dd58888e1359bdfa83621ad2f2c5c
annajliang <annaliangdev@gmail.com>
2021-08-20 00:45:33 -0400

    create js files containing  array of strings for dirty and clean words

---
+++ src/data/dirtyWords.js
@@ -0,0 +1,22 @@
+const dirtyWords = [
+  "hentai",
+  "oppai",
+  "yaoi",
+  "kuso",
+  "ecchi",
+  "yuri",
+  "shounen-ai",
+  "eroge",
+  "fan service",
+  "seme",
+  "uke",
+  "ahegao",
+  "fudanshi",
+  "fujoshi",
+  "futanari",
+  "zettai ryoiki",
+  "sugoi dekai",
+  "mega milk",
+];
+
+export default dirtyWords;
