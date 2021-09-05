fb075691b60acb99190a9328f265bd015e08cd48
annajliang <annaliangdev@gmail.com>
2021-09-05 19:38:14 -0400

    remove index.css import from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import Normalize from "./theme/normalize";
 import GlobalStyles from "./theme/globalStyles";
